2559aa751e8b6eda092c7ffca81754c1331ff474
suzuki <nb@v7.com>
2020-12-01 01:20:00 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisableInsertKey
+{
+    public partial class Form1 : Form
+    {
+        NotifyIcon notifyIcon;
+
+        public bool isDisabledInsertKey = true;
+
+//        DisableInsertKey
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // タスクバーに表示しない。
+            //this.ShowInTaskbar = false;
+
+            this.setComponents();
+
+        }
+
+        private void setComponents()
+        {
+            notifyIcon = new NotifyIcon();
+            // アイコンの設定
+            notifyIcon.Icon = Properties.Resources.red_favicon;
+            // アイコンを表示する
+            notifyIcon.Visible = true;
+            //アイコンにマウスポインタを合わせたときのテキスト
+            notifyIcon.Text = "Insert key is Disabled";
+            // コンテキストメニュー
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            ToolStripMenuItem tsmi1 = new ToolStripMenuItem();
+            ToolStripMenuItem tsmi2 = new ToolStripMenuItem();
+
+            tsmi1.Text = " & Version";
+            tsmi1.Click += ToolStripMenuItem_Version_Click;
+            contextMenuStrip.Items.Add(tsmi1);
+
+            tsmi2.Text = " & Exit";
+            tsmi2.Click += ToolStripMenuItem_End_Click;
+            contextMenuStrip.Items.Add(tsmi2);
+
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+            // NotifyIconのクリックイベント
+            notifyIcon.Click += NotifyIcon_Click;
+        }
+
+        private void ToolStripMenuItem_Version_Click(object sender, EventArgs e)
+        {
+            // バルーンヒントを表示する
+            notifyIcon.BalloonTipTitle = "DisableInsertKey.exe 1.0";
+            notifyIcon.BalloonTipText = "Disable the Insert key when running. Without changing the registry.    twitter.com/DisableInsert";
+            notifyIcon.ShowBalloonTip(10000);
+        }
+
+        private void ToolStripMenuItem_End_Click(object sender, EventArgs e)
+        {
+            //通知アイコンを消去
+            notifyIcon.Visible = false;
+
+            // アプリケーションの終了
+            Application.Exit();
+        }
+
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+
+            var eventArgs = e as MouseEventArgs;
+            switch (eventArgs.Button)
+            {
+                // 左クリック時のみ処理
+                case MouseButtons.Left:
+
+                    // Formの表示/非表示を反転
+                    isDisabledInsertKey = !isDisabledInsertKey;
+
+                    DataProperty p = new DataProperty();
+                    p.IsDisabled = isDisabledInsertKey;
+                    // Console.WriteLine(p.IsDisabled);
+
+                    if (isDisabledInsertKey)
+                    {
+                        notifyIcon.Icon = Properties.Resources.red_favicon;
+                        notifyIcon.Text = "Insert key is Disabled";
+                    }
+                    else
+                    {
+                        notifyIcon.Icon = Properties.Resources.blue_favicon;
+                        notifyIcon.Text = "Insert key is Enabled";
+                    }
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DisableInsertKey
+{
+    public class DataProperty
+    {
+        // 実装は外部から隠蔽(privateにしておく)
+        private static bool InsertKeyStatus; // staticで変数を宣言
+
+        // 変数の取得・変更用のプロパティ
+        public bool IsDisabled
+        {
+            set { InsertKeyStatus = value; }
+            get { return InsertKeyStatus; }
+        }
+    }
+
+
+    class InterceptKeys
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        //DisableInsertKey.Properties.Settings.
+        //public isDisabled = DisableInsertKey.Properties.
+
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+
+            DataProperty p = new DataProperty();
+            p.IsDisabled = true;
+            //Console.WriteLine(p.IsDisabled);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // フォームの表示を抑制するためコメントアウト
+            //Application.Run(new Form1());
+
+            // フォームを表示せずに実行する
+            new Form1();
+
+            _hookID = SetHook(_proc);
+
+            Application.Run();
+
+            UnhookWindowsHookEx(_hookID);
+
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+
+                DataProperty p = new DataProperty();
+
+                int vkCode = Marshal.ReadInt32(lParam);
+                // デバッグ用
+                // Console.WriteLine((Keys)vkCode);
+                // Console.WriteLine(p.IsDisabled);
+
+                // Insertキーが押されたときは入力を破棄
+                if (vkCode == 45 && p.IsDisabled) return new IntPtr(1);
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+
+}
